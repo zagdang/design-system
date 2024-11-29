2294e98b462ac40a44a4201d234d2e0016b05860
KimKyuHoi <k546kh@gmail.com>
2024-11-29 18:41:59 +0900

    env(icons): icons 레포 세팅

---
+++ packages/icons/rollup.config.js
@@ -0,0 +1,34 @@
+import { createRequire } from "module";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+const require = createRequire(import.meta.url);
+const packageJson = require("./package.json");
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      declaration: true,
+      declarationDir: "./dist",
+      outputToFilesystem: true,
+    }),
+  ],
+  external: ["react", "react-dom"],
+};
