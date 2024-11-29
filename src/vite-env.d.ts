203f0cb0e4c594d39448e43457bb177999c5ea1d
KimKyuHoi <k546kh@gmail.com>
2024-11-29 13:21:09 +0900

    env(berry): Vite, EsLint, Prettier세팅

---
+++ src/vite-env.d.ts
@@ -0,0 +1,34 @@
+/// <reference types="vite/client" />
+
+// 이미지
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '*.webp';
+
+// SVG
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+// CSS 모듈
+declare module '*.module.css' {
+  const classes: Readonly<Record<string, string>>;
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: Readonly<Record<string, string>>;
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: Readonly<Record<string, string>>;
+  export default classes;
+}
