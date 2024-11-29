58cf796d99781ab5bc4f7c8d00dd11ee79efa411
KimKyuHoi <k546kh@gmail.com>
2024-11-29 14:27:33 +0900

    env(font): font 환경 세팅하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div className='space-y-4'>
+      <h1 className='text-3xl font-bold'>안녕하세요 (Noto Sans KR Bold)</h1>
+      <p className='text-lg font-normal'>이것은 보통 굵기의 텍스트입니다. (Noto Sans KR Regular)</p>
+      <p className='text-lg font-light'>이것은 얇은 텍스트입니다. (Noto Sans KR Light)</p>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.tsx';
+import './global.css';
+
+createRoot(document.getElementById('root')!).render(<App />);
