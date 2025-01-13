93903f9560d6b399b0234dbf51564294c72b2bcb
KimKyuHoi <k546kh@gmail.com>
2025-01-14 02:36:29 +0900

    fix(coderabbit): coderabbit 제안 내용 수정

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -13,10 +13,10 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
-        fontFamily: {
-          Poppins: ['Poppins', 'sans-serif'],
-        },
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
         card: {
